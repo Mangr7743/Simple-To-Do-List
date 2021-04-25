87e5065a3d7cab1dee93817a6a2f7ac7c2efd45d
Mangr7743 <ravimangat27@gmail.com>
2021-04-25 16:15:25 -0400

    Updated app.js, added Procfile

--- app.js
+++ app.js
@@ -166,6 +166,13 @@ app.post("/delete", function (req, res) {
   
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+// Start listening for prot on heroku or local
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("Server has started successfully");
 });
